9110f7db6e2ee9522ebb76f97444ca1635294155
EmmaLangeland <emml@stud.ntnu.no>
2025-02-17 13:48:45 +0100

    avslutter lab 17.02.25

--- Lab 2/Krysskorrelasjon.py
+++ Lab 2/Krysskorrelasjon.py
@@ -1,22 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sympy import *
-from scipy import signal
+#from sympy import *
+from raspi_import import raspi_import
+import scipy.signal as sc
 
-fs = 3000
 
-x = [0,1,2,3,4]
-y = [2,3,4,5,6]
+#hente inn data fra måling
+sample_period, data = raspi_import('1000Hz_sinus.bin')
+
+#juster data
+k= 3.3/4096 #2^12 pga 12-bits ADC bit_signal = Signal/k
+data = data[5000:,:]*k 
+data = data - np.mean(data, axis=0) #fjerner DC-komponenten til signalet
+
+fs = 1/sample_period
+
+mic_1 = data[:,0] #ADC0
+mic_2 = data[:,2] #ADC2
+mic_3 = data[:,3] #ADC3
 
 #Slower
 #r_xy = np.correlate(x,y, mode='full')
 #r_xy_abs = np.abs(r_xy)
 
 #Faster
-r_xy_s = signal.correlate(x,y, mode='full')
-r_xy_s_abs = np.abs(r_xy_s)
+def krysskorrelasjon(x,y): #sett inn data fra ønskede mikrofoner
+    r_xy = sc.correlate(x,y, mode='full')
+    return r_xy
 
-l_max = np.argmax(r_xy_s_abs) - (len(x) - 1)
-delta_t = l_max/fs
+def delay(x, r_xy): #tar inn parametere x:ett av signalene, r_xy:krysskorrelsjonen
+    r_xy_abs = np.abs(r_xy)
+    l_max = np.argmax(r_xy_abs) - (len(x) - 1)
+    delta_t = l_max/fs
+    print(delta_t)
+    return delta_t
 
-print(delta_t)
+#teori
+t = np.linspace(-5,5, 1000)*np.pi*2
+r_11 = krysskorrelasjon(np.sinc(t+2),np.sinc(t+1))
+
+plt.plot(np.sinc(t))
+plt.plot(np.sinc(t+1))
+plt.plot(r_11)
+plt.show()
+
+
+#med seg selv
+r_11 = krysskorrelasjon(mic_1,mic_3) 
+
+#mellom mikrofonene
+""" r_12 = krysskorrelasjon(mic_1,mic_2)
+r_13 = krysskorrelasjon(mic_1,mic_3)
+r_23 = krysskorrelasjon(mic_1,mic_1) """
+
+print(f"lengden på data: {len(data)}")
+print(f"lengden på krysskorrelasjeonen: {len(r_11)}")
+print(f"delay mellom : {delay(mic_1,r_11)}")
+print(f"spike ved sample nr: {np.argmax(r_11)}")
+plt.plot(r_11)
+plt.show()
--- Lab 2/plot_signal.py
+++ Lab 2/plot_signal.py
@@ -8,32 +8,30 @@ import scipy.signal as sc
 #hente inn data fra måling
 #Sinus_100Hz, Sinus_500Hz, Sinus_1500Hz, Sinus_2000Hz, Sinus_1000Hz
 
-sample_period, data = raspi_import('1000Hz_test3.bin')
+sample_period, data = raspi_import('1000Hz_sinus.bin')
 
 k= 3.3/4096 #2^12 pga 12-bits ADC bit_signal = Signal/k
-data = data[1:,:]*k 
+data = data[5000:,:]*k 
 data = data - np.mean(data, axis=0) #fjerner DC-komponenten til signalet
 
 
 print(data.shape) #(31250, 5)
 print(sample_period) #3.2e-05
 print(np.argmax(data))
-
 print(data.shape[0])
 
-time = np.linspace(0, data.shape[0]*sample_period, data.shape[0])
-#time = np.arange(0, 1, 1/31250)
-#time = np.arange(data.shape[0])*sample_period
-#time= np.arange(0, data.shape[0]*sample_period, sample_period/data.shape[0])
+time = np.linspace(0, (data.shape[0]*sample_period), data.shape[0])
 
 #plotter de rekonstruerte sinussignalene hver for seg med en forskyvning i tid for penere illustrasjon
 """ for i in range(data.shape[1]):
     plt.plot(time+ i/10000, data[:,i], label=f'ADC {i+1}') """
 
-plt.plot(time, data[:,3], label=f'ADC {0}')
+plt.plot(time, data[:,0], label=f'ADC {0}')
+
 plt.xlabel('Tid [s]')
 plt.ylabel('Amplitude [V]')
 plt.title('Samplet sinussignal fra alle 5 AD-konvertere')
 plt.legend(loc='upper right')
 plt.grid()
+#plt.xlim(0,0.01)
 plt.show()
