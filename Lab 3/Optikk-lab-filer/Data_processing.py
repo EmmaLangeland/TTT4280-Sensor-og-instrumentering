248c1bbb50ba1f27e5e2caf61239306c9fbd9bb9
EmmaLangeland <emml@stud.ntnu.no>
2025-03-06 18:37:58 +0100

    legger til oppdatert kode

--- Lab 3/Optikk-lab-filer/Data_processing.py
+++ Lab 3/Optikk-lab-filer/Data_processing.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal as sc
-from scipy import signal  
-import math 
+
 
 # =================== Variables ===================
 filename = "Pulse_test_2.txt"
+fs = 30 #funnet ved å ta lengden på data og dele på tiden spilt inn, også mulig å lese av i terminalen etter kjørt roi.py filen
 
 # =================== Import text file ===================
 data = np.loadtxt(filename)
@@ -16,21 +16,48 @@ Green = data[:,1]
 Blue = data[:,2]
 #Create a time axis
 time = np.arange(len(data))
+print(f"lengden på data i grønt_signal (samples): {len(Green)}")
 
-#Filtrere data??
+
+# =================== Filtrere data med båndpass Butterworth filter ===================
+""" def digitalt_filter(N, low_freq, high_freq, data_2B_filtered): #N er orden på filteret 
+    nyquist = fs / 2
+    low = low_freq / nyquist  #Normaliserer frekvensen
+    high = high_freq / nyquist  
+    filter_coefficients = sc.butter(N, [low, high], "bandpass") #python tupple with coeffs
+    filtrert_data = sc.lfilter(filter_coefficients[0], filter_coefficients[1], data_2B_filtered)
+    return filtrert_data #er et array
+
+Red_filtrert = digitalt_filter(4, 20, , Red)
+Green_filtrert = digitalt_filter(4, 20, 100, Green)
+Blue_filtrert = digitalt_filter(4, 20, 100, Blue)
+ """
 
 
-# Regne ut puls med FFT
+
+def digitalt_filter(N, high_freq, data_2B_filtered): #N er orden på filteret 
+    nyquist = fs / 2  #Normaliserer frekvensen
+    high = high_freq / nyquist  
+    filter_coefficients = sc.butter(N, high, "low") #python tupple with coeffs
+    filtrert_data = sc.lfilter(filter_coefficients[0], filter_coefficients[1], data_2B_filtered)
+    return filtrert_data #er et array
+
+Red_filtrert = digitalt_filter(4, 3, Red)
+Green_filtrert = digitalt_filter(4, 3, Green)
+Blue_filtrert = digitalt_filter(4, 3, Blue)
+
+
+# =================== Regne ut puls med FFT ===================
 def fft(data_kanal):
     Nfft = len(data_kanal)
     data_fft = np.fft.fft(data_kanal, Nfft)
-    return data_fft
+    return np.abs(data_fft)
 
 Green_fft = fft(Green)
 Blue_fft = fft(Blue)
 Red_fft = fft(Red)
 
-# Regne ut puls med autocorrelasjon
+# =================== Regne ut puls med autocorrelasjon ===================
 def autocorrelasjon(data_kanal):
     data_autocorr = sc.correlate(data_kanal, data_kanal, mode = 'full')
     data_autocorr = data_autocorr / np.max(data_autocorr)
@@ -41,18 +68,24 @@ def find_puls_auto(data_kanal):
     peaks_indices = sc.find_peaks(rxy,height = 0.4, threshold=None)
     print(peaks_indices)
 
-find_puls_auto(Green)
-
 Green_autocorr = autocorrelasjon(Green)
 Blue_autocorr = autocorrelasjon(Blue)
 Red_autocorr = autocorrelasjon(Red)
 
 
+
+
 #Plot raw data
-""" plt.plot(Red, "r")
+plt.plot(Red, "r")
 plt.plot(Green, "g")
 plt.plot(Blue, "b")
-plt.show() """
+plt.show()
+
+#Plot av filtrert data
+plt.plot(Red_filtrert, "r")
+plt.plot(Green_filtrert, "g")
+plt.plot(Blue_filtrert, "b")
+plt.show()
 
 #Plot FFT
 plt.plot(Blue_fft, "b")
@@ -65,3 +98,4 @@ plt.plot(Blue_autocorr, "b")
 plt.plot(Red_autocorr, "r")
 plt.plot(Green_autocorr, "g")
 plt.show()
+
