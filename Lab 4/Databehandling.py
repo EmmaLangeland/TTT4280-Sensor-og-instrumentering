effad11ab5bfb4bedf17917af0bae63cbf3625fe
EmmaLangeland <emml@stud.ntnu.no>
2025-03-20 20:17:32 +0100

    snr og litt sånn

--- Lab 4/Databehandling.py
+++ Lab 4/Databehandling.py
@@ -18,7 +18,7 @@ def Hente_data(filnavn):
     data = data - np.mean(data, axis=0) #fjerner DC-komponenten til signalet
 
     time_ax = np.linspace(0, (data.shape[0]*sample_period), data.shape[0])
-    return data, time_ax
+    return data, time_ax, sample_period
 
 
 # CSV
@@ -39,16 +39,24 @@ def Hente_data(filnavn):
 #Kombiner signalene for å få dopler shift
 #Send ut frekvensspekteret med doplershift
 
-def Regn_ut_FFT(data):
-    IF_I = data[:,0]
-    IF_Q = data[:,4]
+def Regn_ut_FFT(data, filnavn):
+    data, time_ax, sample_period = Hente_data(filnavn)
+
+    fs = 1/sample_period
+    IF_I = data[:,0] #ADC0
+    IF_Q = data[:,4] #ADC4
     #Kombiner I og Q kanalene
     IQ = IF_I + 1j*IF_Q
     
     Nfft = len(IQ)
-    Freq_dopler = np.fft.fft(IQ, Nfft)
-    Freq_dopler = Freq_dopler[:len(data)//2]
-    return np.abs(Freq_dopler)
+    FFT_dopler = np.fft.fft(IQ, Nfft)
+    FFT_dopler = np.abs(FFT_dopler)
+    Dopler_shift = np.fft.fftshift(FFT_dopler) #1kHz sample rate
+
+    freqs = np.fft.fftfreq(Nfft, 1/fs)
+    freqs = np.fft.fftshift(freqs)
+    #FFT_dopler = FFT_dopler[:len(data)//2] #Kun halve spekteret
+    return Dopler_shift, freqs
 
 #FFT til db
 
@@ -57,18 +65,66 @@ def FFT_DB(data_FFT):
     data_fft_db_norm = data_fft_db - np.max(data_fft_db)
     return data_fft_db, data_fft_db_norm
 
+def padding(data, pad):
+    padded_data = np.pad(data, (0, pad))
+    return padded_data
+
+
+def PSD(data, filnavn):
+    X, freqs= Regn_ut_FFT(data, filnavn)
+    Effekttetthetsspektrum = (abs(X)**2) #PSD
+    PSD_log = 10*np.log10(Effekttetthetsspektrum)
+    PSD_normalisert = PSD_log - np.max(PSD_log) #normalisering
+    return Effekttetthetsspektrum, PSD_normalisert
+
+
+def SNR(data, filnavn):
+
+    signal_sum = 0
+    noise_sum = 0
+    Effekttetthetsspektrum, norm = PSD(data, filnavn)
+
+    N_sum = 0
+    N_noise = 0
+    for i in range(len(Effekttetthetsspektrum)):
+        if  np.argmax(Effekttetthetsspektrum)-50 < i < np.argmax(Effekttetthetsspektrum)+50:
+            signal_sum += Effekttetthetsspektrum[i]
+            N_sum += 1
+        else:
+            noise_sum += Effekttetthetsspektrum[i]
+            N_noise += 1
+
+    print(signal_sum)
+    print(noise_sum)
+    print(Effekttetthetsspektrum[0])
+    print(Effekttetthetsspektrum[1])
+    signal_sum_norm = signal_sum/N_sum
+    noise_sum_norm = noise_sum / N_noise
+    SNR = 10*np.log10(np.abs(signal_sum_norm / noise_sum_norm))
+
+    return SNR
+
+def radiell_hastighet(fft_dopler, freqs):
+    index = np.argmax(fft_dopler)
+    f_d = freqs[index]
+    print(f_d)
+    #f_d = np.abs(f_d)
+    f0 = 24*10**9 #Hz
+    c = 3*10**8
+    v_r = c*f_d/(2*f0)
+    return v_r
 
 
 #======================= Plot Data =======================
 
 def Plot_raw(data):
-    plt.plot(data[:,0])
+    plt.plot(data[:,0], label='ADC0')
     plt.show()
-    plt.plot(data[:,4])
+    plt.plot(data[:,4], label='ADC4')
     plt.show()
 
-def Plot_FFT(data):
-    plt.plot(data)
+def Plot_FFT(data, freqs):
+    plt.plot(freqs, data)
 
 #********************************************************
 #======================== MAIN ==========================
@@ -76,18 +132,32 @@ def Plot_FFT(data):
 
 def MAIN(filnavn):
     #Hente ut data:
-    data, time_ax = Hente_data(filnavn)
+    data, time_ax, s = Hente_data(filnavn)
     #data = Hente_data_CSV(filnavn)
 
     #Regne ut FFT:
-    Freq_FFT = Regn_ut_FFT(data)
+    FFT_dopler, freqs = Regn_ut_FFT(data, filnavn)
 
     #Plot DATA ------------------
-    Plot_raw(data)
-    #Plot_FFT(Freq_FFT)
+    #Plot_raw(data)
+    Plot_FFT(FFT_dopler, freqs)
     plt.show()
 
+    #FFT til db
+    FFT_dopler_db, FFT_dopler_db_norm = FFT_DB(FFT_dopler)
+    Plot_FFT(FFT_dopler_db, freqs)
+    plt.show()
+
+    #SNR(FFT_dopler, filnavn)
+    #print("SNR: ", SNR(FFT_dopler, filnavn))
+
+    padded_fft = padding(FFT_dopler_db, 2**16)
+    plt.plot(padded_fft)
+    plt.show()
+
+    v = radiell_hastighet(FFT_dopler, freqs)
+    print(v)
 
 #================ Kjør Programmet =================
 
-MAIN("fram_speed_5.bin")
+MAIN("bak_1.bin")
